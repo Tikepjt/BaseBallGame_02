3cf0326bd96408ec9967710d6fd2a0a5ad458933
tikepjt <tikepjt@gmail.com>
2018-08-02 17:52:42 +0900

    first commit

---
+++ app/src/main/java/com/example/summit/baseballgame_02/IntroActivity.java
@@ -0,0 +1,16 @@
+package com.example.summit.baseballgame_02;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
